8b8bdccdca3704dbe0c4bb99c5e568bf71e194bf
orio <orio.dev.br@gmail.com>
2026-03-09 16:30:46 -0300

    feat(ui): update loading state to a bouncing shopping bag

--- frontend/src/components/LoadingState.tsx
+++ frontend/src/components/LoadingState.tsx
@@ -1,16 +1,13 @@
+import { ShoppingBag } from 'lucide-react';
 import './LoadingState.css';
 
 export const LoadingState = () => {
     return (
         <div className="loading-container">
             <div className="loading-content">
-                <div className="cube-loader">
-                    <div className="cube-top"></div>
-                    <div className="cube-bottom"></div>
-                    <div className="cube-left"></div>
-                    <div className="cube-right"></div>
-                    <div className="cube-front"></div>
-                    <div className="cube-back"></div>
+                <div className="bag-loader">
+                    <ShoppingBag size={64} className="loading-icon" />
+                    <div className="loading-shadow"></div>
                 </div>
                 <h3 className="loading-text">Carregando as melhores ofertas...</h3>
             </div>
