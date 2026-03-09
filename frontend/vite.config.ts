c025ff68c41a3830cce445f5a218bfde26576356
orio <orio.dev.br@gmail.com>
2026-03-09 12:38:19 -0300

    fix: rename pwa icons to bypass cache

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,13 +17,13 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: '/pwa-192x192.png',
+            src: '/app-icon-192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any maskable'
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/app-icon-512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
