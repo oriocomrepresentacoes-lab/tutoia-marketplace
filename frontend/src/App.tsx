afa5d589d5e20d5f2bd723a219299e36a09a7734
orio <orio.dev.br@gmail.com>
2026-03-12 15:54:14 -0300

    trigger: Force Vercel rebuild

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+// Deployment Trigger - Edit Ad Fix
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
